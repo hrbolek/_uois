de52988607c66f69d3fc5a88a3fcdaf712c332ca
Profesor Hrbolek <profesor@hrbolek.cz>
2022-11-22 22:39:25 +0100

    Timetable experiments

--- frontend/src/pages/user/teacher.js
+++ frontend/src/pages/user/teacher.js
@@ -143,9 +143,8 @@ export const SVGTime = (props) => {
     const { Xdelta, HourValue } = props
     return (
         <>
-            <rect x={Xdelta} y="23" width="220" height="25" stroke="#000000" stroke-width="1.33333" stroke-miterlimit="8"></rect>
-            <rect x={Xdelta} y="23" width="220" height="25" stroke="#FFFFFF" stroke-width="1.33333" stroke-miterlimit="8" fill="#FFFFFF"></rect>
-            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="18" transform={"translate(" + (Xdelta + 5) +" 43)"}>
+            <rect x={Xdelta} y="-25" width="180" height="25" stroke="#000000" stroke-width="1" fill="#FFFFFF"></rect>
+            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="18" transform={"translate(" + (Xdelta + 5) +" -10)"}>
                 {HourValue}
             </text>
         </>
@@ -156,11 +155,10 @@ export const SVGDate = (props) => {
     const { YDelta, DateValue, DayValue } = props
     return (
         <>
-            <rect x="18" y={YDelta} width="60" height="100" stroke="#000000" stroke-width="1.33333" stroke-miterlimit="8"></rect>
-            <rect x="18" y={YDelta} width="60" height="100" stroke="#FFFFFF" stroke-width="1.33333" stroke-miterlimit="8" fill="#FFFFFF"></rect>
-            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="18" transform={"translate(23 " + (YDelta + 20) + ")"}>
+            <rect x="-60" y={YDelta} width="60" height="100" stroke="#000000" stroke-width="1" fill="#FFFFFF"></rect>
+            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="18" transform={"translate(-58 " + (YDelta + 20) + ")"}>
                 {DateValue}
-                <tspan font-size="15" font-weight="400" x="-0.0424118" y="23">{DayValue}</tspan>
+                <tspan font-size="18" font-weight="400" x="-0.0424118" y="23">{DayValue}</tspan>
             </text>
         </>
     )
@@ -170,59 +168,54 @@ export const SVGEvent = (props) => {
     const { XDelta, YDelta, fillColor, lesson, subject, teacher, room } = props
     return (
         <>
-            <rect x={XDelta} y={YDelta} width="220" height="100" stroke="#000000" stroke-width="1.33333" stroke-miterlimit="8"></rect>
-            <rect x={XDelta} y={YDelta} width="220" height="100" stroke="#f7b4b7" stroke-width="1.33333" stroke-miterlimit="8" fill={fillColor}></rect>
-            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="18" transform={"translate(" + (XDelta + 5) + " " + (YDelta + 20) +")"}>
+            <rect x={XDelta} y={YDelta} width="180" height="100" stroke="#f7b4b7" stroke-width="1" fill={fillColor}></rect>
+            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="15" transform={"translate(" + (XDelta + 5) + " " + (YDelta + 14) +")"}>
                 <a href={"/ui/studyprograms/subject/" + lesson.id} target="_top">{lesson.name}</a>
-                <tspan font-size="15" font-weight="400" x="-0.0424118" y="23"><a href={"/ui/studyprograms/subject/" + subject.id} target="_top"></a>{subject.name}</tspan>
-                <tspan font-size="15" font-weight="400" x="1.04089" y="45"><a href={"/ui/users/teacher/" + teacher.id} target="_top">{teacher.name + " " + teacher.surname }</a></tspan>
-                <tspan font-size="15" font-weight="400" x="1.04089" y="70"><a href={"/ui/areals/room/" + room.id} target="_top">{room.name}</a></tspan>
+                <tspan font-size="15" font-weight="400" x="-0.0424118" y="18"><a href={"/ui/studyprograms/subject/" + subject.id} target="_top"></a>{subject.name}</tspan>
+                <tspan font-size="15" font-weight="400" x="1.04089" y="38"><a href={"/ui/users/teacher/" + teacher.id} target="_top">{teacher.name + " " + teacher.surname }</a></tspan>
+                <tspan font-size="15" font-weight="400" x="1.04089" y="58"><a href={"/ui/areals/room/" + room.id} target="_top">{room.name}</a></tspan>
+                <tspan font-size="15" font-weight="400" x="1.04089" y="78"><a href={"/ui/areals/room/" + room.id} target="_top">skupina</a></tspan>
             </text>
         </>
     )
 }
 
 export const SVGTimeTable = (props) => {
-  /*  return (
-        <svg style={{"display": "inline-block",	"width": "100%"}} viewBox="0 0 1280 720" preserveAspectRatio="xMinYMid" width="1280" height="720" xmlns="http://www.w3.org/2000/svg" overflow="hidden">
-        <rect x="1" y="1" width="1280" height="720" style={{"fill":"rgb(127,127,127)", "strokeWidth":"3", "stroke": "rgb(0,0,0)"}} />
-        <rect x="50" y="20" width="300" height="100" style={{"fill":"rgb(127,127,255)", "strokeWidth":"3", "stroke": "rgb(0,0,0)"}} />
-    </svg>
-
-    )
-*/
+    //meritko je v ose x jedna minuta         -> 2 body
+    //           v ose y jeden den            -> 100 bodu
+    //referencnim bodem je cas 8:00 prvni den -> (0, 0)
     return (
-        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" overflow="hidden" viewBox="10 20 1400 550">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" overflow="hidden" viewBox="-61 -26 1400 550">
          <g>
-                <SVGTime Xdelta={80} HourValue={"8:00"} />
-                <SVGTime Xdelta={302} HourValue={"9:50"} />
-                <SVGTime Xdelta={524} HourValue={"11:40"} />
-                <SVGTime Xdelta={746} HourValue={""} />
-                <SVGTime Xdelta={968} HourValue={"14:30"} />
-                <SVGTime Xdelta={1190} HourValue={"16:20"} />
+                <SVGTime Xdelta={0} HourValue={"8:00"} />
+                <SVGTime Xdelta={220} HourValue={"9:50"} />
+                <SVGTime Xdelta={440} HourValue={"11:40"} />
+                <SVGTime Xdelta={780} HourValue={"14:30"} />
+                <SVGTime Xdelta={1000} HourValue={"16:20"} />
+                <SVGTime Xdelta={1200} HourValue={"18:00"} />
 
-                <SVGDate YDelta={50} DateValue={"8.11."} />
-                <SVGDate YDelta={152} DateValue={"9.11."} />
-                <SVGDate YDelta={254} DateValue={"10.11."} />
-                <SVGDate YDelta={356} DateValue={"11.11."} />
-                <SVGDate YDelta={458} DateValue={"12.11."} />
+                <SVGDate YDelta={0} DateValue={"8.11."} DayValue={"po"}/>
+                <SVGDate YDelta={100} DateValue={"9.11."} DayValue={"út"}/>
+                <SVGDate YDelta={200} DateValue={"10.11."} DayValue={"st"}/>
+                <SVGDate YDelta={300} DateValue={"11.11."} DayValue={"čt"}/>
+                <SVGDate YDelta={400} DateValue={"12.11."} DayValue={"pá"}/>
 
-                <SVGEvent XDelta={968} YDelta={356} fillColor={"#f7b4b7"}
+                <SVGEvent XDelta={780} YDelta={300} fillColor={"#f7b4b7"}
                     lesson={{name: "konzultace", id: "1"}} 
                     subject={{name: "", id: "1"}} 
                     teacher={{name: "Michal", surname: "Svoboda", id: "1"}} 
                     room={{name: "", id: "1"}} />
-                <SVGEvent XDelta={302} YDelta={152} fillColor={"#b6bd82"}
+                <SVGEvent XDelta={220} YDelta={100} fillColor={"#b6bd82"}
                     lesson={{name: "Letecké elektronické systémy", id: "1"}} 
                     subject={{name: "3. Syntéza kmitočtu", id: "1"}} 
                     teacher={{name: "Michal", surname: "Svoboda", id: "1"}} 
                     room={{name: "Č1/120", id: "1"}} />
-                <SVGEvent XDelta={302} YDelta={458} fillColor={"#b6bd82"}
+                <SVGEvent XDelta={220} YDelta={400} fillColor={"#b6bd82"}
                     lesson={{name: "Letecké elektronické systémy", id: "1"}} 
                     subject={{name: "3. Syntéza kmitočtu", id: "1"}} 
                     teacher={{name: "Michal", surname: "Svoboda", id: "1"}} 
                     room={{name: "Č1/120", id: "1"}} />
-                <SVGEvent XDelta={80} YDelta={356} fillColor={"#859be9"}
+                <SVGEvent XDelta={0} YDelta={300} fillColor={"#859be9"}
                     lesson={{name: "Mentoring studentů 1. ročníku", id: "1"}} 
                     subject={{name: "", id: "1"}} 
                     teacher={{name: "Jana", surname: "Svobodová", id: "1"}} 
@@ -233,6 +226,71 @@ export const SVGTimeTable = (props) => {
         </svg>        
     )
 }
+
+export const SheetA4Week = (props) => {
+    const { XDelta, YDelta } = props
+    const days = [0, 1, 2, 3, 4];
+    const dayHeight = 200
+    return (
+        <>
+            <rect x={XDelta} y={YDelta} width="20" height={dayHeight * days.length} stroke="#000000" stroke-width="1" fill="#FFFFFF">
+            </rect>
+            {days.map((item, index) => 
+                <SheetA4WeekDay XDelta={XDelta} YDelta={YDelta + index * dayHeight} DateValue={item}/>
+            )}
+        </>
+    )
+}
+
+export const SheetA4WeekDayLesson = (props) => {
+    const { XDelta, YDelta, DateValue } = props
+    return (
+        <>
+            <rect x={XDelta} y={YDelta} width="20" height="30" stroke="#000000" stroke-width="1" fill="#FFFFFF"></rect>
+            <text font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="600" font-size="5" transform={"translate(" + (XDelta + 1) + " " + (YDelta + 4) +")"}>
+                <a href={"/ui/studyprograms/subject/"} target="_top">lssN</a>
+                <tspan font-size="5" font-weight="400" x="0" y="7"><a href={"/ui/studyprograms/subject/"} target="_top"></a>SUB</tspan>
+                <tspan font-size="5" font-weight="400" x="0" y="12"><a href={"/ui/users/teacher/"} target="_top">TEA</a></tspan>
+                <tspan font-size="5" font-weight="400" x="0" y="18"><a href={"/ui/areals/room/"} target="_top">ROOM</a></tspan>
+                <tspan font-size="5" font-weight="400" x="0" y="24"><a href={"/ui/areals/room/"} target="_top">GRP</a></tspan>
+            </text>
+        </>
+
+    )
+}
+
+export const SheetA4WeekDay = (props) => {
+    const { XDelta, YDelta, DateValue } = props
+    const hours = [0, 1, 2, 3, 4, 5]
+    return (
+        <>
+        <rect x={XDelta} y={YDelta} width="20" height="20" stroke="#000000" stroke-width="1" fill="#ffcc99"></rect>
+        {hours.map((item, index) => <SheetA4WeekDayLesson XDelta={XDelta} YDelta={YDelta + 20 + 30 * index} />)}
+        </>
+    )
+}
+
+export const SheetA4Date = (props) => {
+    const { YDelta } = props
+    return (
+        <rect x="-60" y={YDelta} width="60" height="100" stroke="#000000" stroke-width="1" fill="#FFFFFF"></rect>
+    )
+}
+
+export const SheetA4TimeTable = (props) => {
+    const weeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
+    return (
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" overflow="hidden" viewBox="-61 -26 500 1400">
+         <g>
+             <SheetA4Week XDelta={-50} YDelta={0} />
+            <SheetA4Week XDelta={-30} YDelta={0} />
+             {weeks.map((item, index) => <SheetA4Week XDelta={index * 20} YDelta={0} /> )}
+         </g>
+        </svg> 
+    )
+}
+
+//                <SVGTimeTable />
 
 export const TeacherSmallTimeTable = (props) => {
     return (
@@ -245,7 +303,7 @@ export const TeacherSmallTimeTable = (props) => {
                 </Card.Title>
             </Card.Header>
             <Card.Body>
-                <SVGTimeTable />
+                <SheetA4TimeTable />
             </Card.Body>
         </Card>
     )
